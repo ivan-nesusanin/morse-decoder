cf8144fb4c4f5dc6d19d52ffdd0a9ad50120f3da
ivan-nesusanin <l75x5@yandex.ru>
2021-08-18 22:33:25 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,8 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  let arrOf10 = [];
+  let decodeStr = '';
+  for (let i = 0; i < expr.length; i += 10) {
+    arrOf10.push(expr.substring(i, i+10));
+  }
+  for (let i = 0; i < arrOf10.length; i++) {
+    if (arrOf10[i] === '**********') {
+      decodeStr += ' ';
+    } else {
+      let arrOf2 = [];
+      for (let j = 0; j < arrOf10[i].length; j += 2) {
+        arrOf2.push(arrOf10[i].substring(j, j+2));
+      }
+      let morseLet = '';
+      for (let i = 0; i < arrOf2.length; i++) {
+        switch (arrOf2[i]) {
+          case '00':
+            break;
+          case '10':
+            morseLet += '.'
+            break;
+          case '11':
+            morseLet += '-'
+            break;
+        }
+      }
+      // if (morseLet in MORSE_TABLE)
+      decodeStr += MORSE_TABLE[morseLet]
+    }
+  }
+  return decodeStr;
+};
 
 module.exports = {
     decode
